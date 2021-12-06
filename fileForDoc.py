8f1b7259bb4334c14ba67a6404421b02648e92ad
mdsaifkhan20001 <95009445+mdsaifkhan20001@users.noreply.github.com>
2021-12-06 23:16:05 +0530

    Update fileForDoc.py

--- fileForDoc.py
+++ fileForDoc.py
@@ -1,91 +1,66 @@
-# necessary Imports
+# importing the necessary dependencies
+from flask_cors import CORS,cross_origin
+from sklearn.ensemble import RandomForestRegressor
+#import sklearn.linear_model.LinearRegression
+from flask import Flask, request, jsonify,render_template
+import os
+from wsgiref import simple_server
+from sklearn.linear_model import LinearRegression
+import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
-#from main import categorical_to_numeric
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split, GridSearchCV
-from scipy import stats 
-from sklearn.metrics import mean_squared_error
-import pickle
+
+app = Flask(__name__) # initializing a flask app
+
+@app.route('/',methods=['GET'])  # route to display the home page
+@cross_origin()
+def homePage():
+    return render_template("index.html")
+
+@app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
+@cross_origin()
+def index():
+    if request.method == 'POST':
+        try:
+        
+            #  reading the inputs given by the user
+            season=float(request.form['season'])
+            holiday = float(request.form['holiday'])
+            workingday = float(request.form['workingday'])
+            weather = float(request.form['weather'])
+            temp = float(request.form['temp'])
+            atemp = float(request.form['atemp'])
+            humidity = float(request.form['humidity'])
+            windspeed = float(request.form['windspeed'])
+            casual = float(request.form['casual'])
+            registered = float(request.form['registered'])
+            hour = float(request.form['hour'])
+            #is_research = request.form['research']
+            #if(is_research=='yes'):
+                #research=1
+            #else:
+                #research=0
+            filename = 'finalized_new_bike_model.pickle'
+            RF1 = pickle.load(open(filename, 'rb')) # loading the model file from the storage
+            # predictions using the loaded model file
+            #prediction=loaded_model.predict([[gre_score,toefl_score,university_rating,sop,lor,cgpa,research]])
+            prediction=RF1.predict([[season,holiday,workingday,weather,temp,atemp,humidity,windspeed,casual,registered,hour]])
+            print('prediction is', prediction)
+            # showing the prediction results in a UI
+            #return render_template('results.html')
+            return render_template('results.html',prediction=np.round(prediction))
+        except Exception as e:
+            print('The Exception message is: ',e)
+            return 'something is wrong'
+    # return render_template('results.html')
+    else:
+        return render_template('index.html')
 
 
-#test = pd.read_csv('train_bikes.csv', parse_dates=['datetime'])
-train= pd.read_csv('new_train.csv') # loading the training data
-
-print(train.head())
-
-Y = train['count']
-X = train.drop(columns = ['count'])
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, Y,test_size=0.25,random_state=255
-)
-print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
-
-Rf = RandomForestRegressor(n_estimators = 100,criterion='mse',random_state=255,max_depth=50,min_samples_split=10, verbose=3,oob_score=True)
-Rf.fit(X_train,y_train)
-
-print(Rf.score(X_test,y_test))
-
-Rf.predict([[1,0,0,1,9.84,14.395,81,0.0,3,13,0]])
-
-
-# saving the model to the local file system
-filename = 'finalized_new1_bike_model.pickle'
-pickle.dump(Rf, open(filename, 'wb'))
-
-filename = 'finalized_new1_bike_model.pickle'
-RF1 = pickle.load(open(filename, 'rb'))
-pred=RF1.predict([[1,0,1,1,10.66,13.65,35,7.0015,12,55,2]])
-print('prediction is', pred)
-
-'''df= pd.read_csv('Admission_Prediction.csv') # reading the CSV file
-
-df.head() # cheking the first five rows from the dataset
-df.info() # printing the summary of the dataframe
-
-df['GRE Score'].fillna(df['GRE Score'].mode()[0],inplace=True)
-#to replace the missing values in the 'GRE Score' column with the mode of the column
-# Mode has been used here to replace the scores with the most occuring scores so that data follows the general trend
-
-df['TOEFL Score'].fillna(df['TOEFL Score'].mode()[0],inplace=True)
-#to replace the missing values in the 'GRE Score' column with the mode of the column
-# Mode has been used here to replace the scores with the most occuring scores so that data follows the general trend
-
-df['University Rating'].fillna(df['University Rating'].mean(),inplace=True)
-#to replace the missing values in the 'University Rating' column with the mode of the column
-# Mean has been used here to replace the scores with the average score
-
-# dropping the 'Chance of Admit' and 'serial number' as they are not going to be used as features for prediction
-x=df.drop(['Chance of Admit','Serial No.'],axis=1)
-# 'Chance of Admit' is the target column which shows the probability of admission for a candidate
-y=df['Chance of Admit']
-
-
-plt.scatter(df['GRE Score'],y) # Relationship between GRE Score and Chance of Admission
-plt.scatter(df['TOEFL Score'],y) # Relationship between TOEFL Score and Chance of Admission
-plt.scatter(df['CGPA'],y) # Relationship between CGPA and Chance of Admission
-
-
-# splitting the data into training and testing sets
-from sklearn.model_selection import train_test_split
-train_x,test_x,train_y,test_y=train_test_split(x,y,test_size=0.33, random_state=100)
-
-# fitting the date to the Linear regression model
-from sklearn import linear_model
-reg = linear_model.LinearRegression()
-reg.fit(train_x, train_y)
-
-# calucltaing the accuracy of the model
-from sklearn.metrics import r2_score
-score= r2_score(reg.predict(test_x),test_y)
-
-# saving the model to the local file system
-filename = 'finalized_model.pickle'
-pickle.dump(reg, open(filename, 'wb'))
-
-# prediction using the saved model.
-loaded_model = pickle.load(open(filename, 'rb'))
-prediction=loaded_model.predict(([[320,120,5,5,5,10,1]]))
-print(prediction[0])'''
+if __name__ == "__main__":
+    port = int(os.getenv("PORT"))
+    #clApp = ClientApp()
+    host = '0.0.0.0'
+    httpd = simple_server.make_server(host=host,port=port, app=app)
+    httpd.serve_forever()
